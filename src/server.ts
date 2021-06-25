58c312b9cc5207f1aa7efe9d6c56135236ca14f5
JulioAvalos <julioavalos2@msn.com>
2021-06-24 23:24:10 -0600

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+// console.log("Hola a la academia online!!");
+
+import express from "express";
+import compression from "compression";
+import cors from "cors";
+import { createServer } from "http";
+
+const app = express();
+
+app.use(cors());
+app.use(compression());
+
+app.get("/", (req, res) => {
+    res.send("Hola a la academia online en GraphQL");
+});
+
+const httpServer = createServer(app);
+const PORT = 5200;
+
+httpServer.listen(
+    {
+        port: PORT,
+    },
+    ()=> {
+        console.log(`Servidor academia online list -> http://localhost:${PORT}`);
+    }
+);
