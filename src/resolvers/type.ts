16836027dec87978555bb1ec174e42eb9d220aed
JulioAvalos <julioavalos2@msn.com>
2021-06-29 00:40:56 -0600

    querying for student lists from courses

--- src/resolvers/type.ts
+++ src/resolvers/type.ts
@@ -6,11 +6,24 @@ const type: IResolvers = {
     Estudiante: {
         courses: parent => {
             const cursosLista: Array<any> = [];
-            parent.courses.map((curso: string) => {
+            parent.courses.forEach((curso: string) => {
                 cursosLista.push(_.filter(database.cursos, ["id", curso])[0]);
             });
             return cursosLista;
         }
+    },
+    Curso: {
+       students: parent => {
+           const listaEstudiantes: Array<any> = [];
+           const idCurso = parent.id;
+           database.estudiantes.forEach((estudiante: any) => {
+               if(estudiante.courses.filter((id: any) => id === idCurso) > 0) {
+                   listaEstudiantes.push(estudiante);
+               } 
+           });
+           return listaEstudiantes;
+       },
+       path: parent => `https://www.udemy.com${parent.path}`
     }
 };
 
